dc80fe3c3194366d43f491fa443dae064c32ebbe
vasconcelos <di0g0vasconcelos@hotmail.com>
2024-06-23 16:43:54 -0300

    creation of domain, ifra, repository and services

--- golang/go.mod
+++ golang/go.mod
@@ -2,4 +2,10 @@ module github.com/devsvasconcelos/dev-ticket/golang
 
 go 1.22.4
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/devfullcycle/imersao18/golang v0.0.0-20240621151719-0a883bd7a8d5
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/google/uuid v1.6.0
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- golang/internal/events/domain/ticket.go
+++ golang/internal/events/domain/ticket.go
@@ -1,6 +1,10 @@
 package domain
 
+import "errors"
+
 type TicketType string
+
+var ErrTicketPriceZero = errors.New("ticket price must be greater tha")
 
 const (
 	TicketTypeHaf  TicketType = "half"
@@ -14,4 +18,21 @@ type Ticket struct {
 	Status     SpotStatus
 	TicketType TicketType
 	Price      float64
+}
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHaf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) calculatePrice() {
+	if t.TicketType == TicketTypeHaf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceZero
+	}
+	return nil
 }
