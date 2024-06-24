407ce2e6b8cba91eab57aafd9ba7e465da996f42
vasconcelos <di0g0vasconcelos@hotmail.com>
2024-06-24 18:09:15 -0300

    add fixture and change in service

--- nestjs-partners-api/apps/partner1/src/partner1.module.ts
+++ nestjs-partners-api/apps/partner1/src/partner1.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { EventsModule } from '../events/events.module';
-import { SpotsModule } from '../spots/spots.module';
+import { EventsModule } from './events/events.module';
+import { SpotsModule } from './spots/spots.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@app/core/prisma/prisma.module';
 import { AuthModule } from '@app/core/auth/auth.module';
